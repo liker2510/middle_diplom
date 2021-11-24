660c409e23b3f88ac045452241e40360a69a08e3
Ilker Karakurt <i_love_history96@mail.ru>
2021-11-25 00:05:40 +0500

    валидация

--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
 import smootScroll from "./modules/smootScroll";
 import popup from "./modules/popup";
+import vaidate from "./modules/validate";
+// import sendForm from "./modules/sendForm";
 
 smootScroll();
 popup();
+vaidate();
+// sendForm({
+//     formId: 'form1',
+//     someElem: [
+//         {
+//             type: 'block',
+//             id: 'total'
+//         }
+//     ]
+// });
+// sendForm({
+//     formId: 'form2',
+//     someElem: [
+//         {
+//             type: 'block',
+//             id: 'total'
+//         }
+//     ]
+// });
