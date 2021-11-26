4492908fd4113e7698d4102aabdbead34f434d02
Ilker Karakurt <i_love_history96@mail.ru>
2021-11-26 21:33:45 +0500

    Слайдер benefits

--- src/modules/slider.js
+++ src/modules/slider.js
@@ -3,23 +3,59 @@ const slider = () => {
     const benefitsItem = document.querySelectorAll('.benefits__item');
     let screenWidth = window.screen.width
 
-    if (screenWidth > 576) {
-        benefitsItem.forEach((index) => {
-            index.classList.add('dnone');
-        })
+    let currentSlide = 0;
+
+    const prevSlide = (elems, index, strClass) => {
+        elems[index].classList.remove(strClass);
     }
 
-    // if (benefits !== null) {
-    //     console.log(services);
-    //     console.log(sliderServiceBlock);
-    //     sliderServiceBlock.forEach((index) => {
-    //         screenWidth = window.screen.width
-    //         if (screenWidth > 576) {
-    //             index.classList.add('dnone');
-    //         }
-  
-    //     })
-    // }
+    const nextSlide = (elems, index, strClass) => {
+        elems[index].classList.add(strClass);
+    }
+
+    if (screenWidth <= 576) {
+        benefitsItem.forEach((index) => {
+            index.classList.add('dnone');
+            index.style = 'max-width: 100%;';
+            benefitsItem[0].classList.add('service-block-active');
+        })
+
+        benefits.addEventListener('click', (e) => {
+            prevSlide(benefitsItem, currentSlide, 'service-block-active')
+            if (e.target.matches('.benefits__arrow--right>img')) {
+                currentSlide++
+            } else if (e.target.matches('.benefits__arrow--left>img')) {
+                currentSlide--
+            }
+            if (currentSlide >= benefitsItem.length) {
+                currentSlide = 0
+            }
+            if (currentSlide < 0) {
+                currentSlide = benefitsItem.length - 1
+            }
+            nextSlide(benefitsItem, currentSlide, 'service-block-active');
+        })
+    } else {
+        benefitsItem.forEach((index) => {
+            index.classList.add('dnone');
+            index.style = 'max-width: 100%;';
+            benefitsItem[0].classList.add('service-block-active');
+            benefitsItem[1].classList.add('service-block-active');
+            benefitsItem[2].classList.add('service-block-active');
+        })
+
+        benefits.addEventListener('click', (e) => {
+            if (e.target.matches('.benefits__arrow--right>img')) {
+                benefitsItem.forEach((index) => {
+                    index.classList.toggle('service-block-active');
+                })
+            } else if (e.target.matches('.benefits__arrow--left>img')) {
+                benefitsItem.forEach((index) => {
+                    index.classList.toggle('service-block-active');
+                })
+            }
+        })
+    }
 
 }
 
